c74ee81158554c1566d02fba4888e6d67d7d8aa8
Vengi75 <JVZ@meggido.co.za>
2018-10-22 18:48:32 +0200

    Initial Project Commit

---
+++ src/LuckyChloePRJ.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : LuckyChloePRJ.cpp
+// Author      : Jaques van Zyl - Vengi57
+// Version     :
+// Copyright   : Your copyright notice
+// Description :  Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "HI" << endl; // prints HI
+	return 0;
+}
